557a9c9640ed81d34e430d15c90f55cae3677d01
Alfredo <icealfredo123@gmail.com>
2022-09-17 11:35:36 -0500

    1

---
+++ hackSMU/CameraLocation.swift
@@ -0,0 +1,20 @@
+//
+//  CameraLocation.swift
+//  hackSMU
+//
+//  Created by Alfredo Gutierrez on 9/17/22.
+//
+
+import SwiftUI
+
+struct CameraLocation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CameraLocation_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraLocation()
+    }
+}
--- hackSMU/ContentView.swift
+++ hackSMU/ContentView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        
+        
+        
         ZStack{
             Color("natureGreen")
                 .ignoresSafeArea()
@@ -18,12 +22,12 @@ struct ContentView: View {
                     .ignoresSafeArea(edges: .top)
                     .frame(height: 400)
                     
-
+                
                 VStack(alignment: .leading) {
                     Text("Creating a Sustainable Future")
                         .font(.title)
                     Divider()
-
+                    
                     VStack {
                         Text("The 21st century has lead to astronomical technological developments at an exponential rate.")
                             .font(.subheadline)
@@ -39,13 +43,29 @@ struct ContentView: View {
                             .font(.subheadline)
                             .multilineTextAlignment(.center)
                         Divider()
-
                     }
                     .foregroundColor(.secondary)
+         
+                
                 }
                 .padding()
                 Spacer()
-
+                
+                NavigationView
+                {
+                    Button(action: {
+                        print("hello world")
+                    }){
+                        Text("Open Camera")
+                            .font(.title)
+                            .padding()
+                            .foregroundColor(.black)
+                            .background(Color.blue)
+                            .cornerRadius(30)
+                    
+                    }
+                }
+          
             }
         }
     }
